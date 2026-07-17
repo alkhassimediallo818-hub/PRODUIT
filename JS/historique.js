1e53a26376267553741d2c72d44d2ab23cff7d54
Qassimedev <alkhassimediallo818@gmail.com>
2026-07-17 11:08:06 +0000

    historique.js

---
+++ JS/historique.js
@@ -0,0 +1,78 @@
+// ===============================
+// HISTORIQUE
+// ===============================
+
+
+import {
+    db,
+    auth
+} from "./firebase.js";
+
+
+import {
+    collection,
+    addDoc,
+    getDocs,
+    deleteDoc,
+    doc,
+    query,
+    where,
+    serverTimestamp
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
+
+import {
+    nettoyerTexte,
+    utilisateurValide
+} from "./utils.js";
+
+
+
+
+// ===============================
+// ENREGISTRER HISTORIQUE
+// ===============================
+
+export async function enregistrerHistorique(
+    utilisateurConnecte,
+    type,
+    produit
+){
+
+
+    if(!utilisateurValide(auth, utilisateurConnecte))
+        return;
+
+
+
+    await addDoc(
+
+        collection(db,"historique"),
+
+        {
+
+            userId:
+            auth.currentUser.uid,
+
+
+            type:
+            nettoyerTexte(type)
+            ||
+            "action",
+
+
+            produit:
+            nettoyerTexte(produit)
+            ||
+            "Inconnu",
+
+
+            date:
+            serverTimestamp()
+
+        }
+
+    );
+
+
+}
