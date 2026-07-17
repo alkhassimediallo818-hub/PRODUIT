364d2d3509d1616e658345db52a4bab804f14518
Qassimedev <alkhassimediallo818@gmail.com>
2026-07-17 10:55:21 +0000

    produits.js

--- JS/produits.js
+++ JS/produits.js
@@ -36,7 +36,137 @@ let produitModification = null;
 
 
 
-export {
-    produits,
-    produitModification
-};
+export function getProduits(){
+
+    return produits;
+
+}
+
+
+
+export async function chargerProduits(utilisateurConnecte){
+
+
+    if(!utilisateurValide(auth, utilisateurConnecte))
+        return;
+
+
+
+    produits = [];
+
+
+    const q = query(
+
+        collection(db,"produits"),
+
+        where(
+            "userId",
+            "==",
+            auth.currentUser.uid
+        )
+
+    );
+
+
+
+    const snapshot =
+    await getDocs(q);
+
+
+
+    snapshot.forEach((docSnap)=>{
+
+
+        const data =
+        docSnap.data();
+
+
+
+        produits.push({
+
+            id:
+            docSnap.id,
+
+            ...data,
+
+            stockTotal:
+            nombreValide(data.stockTotal),
+
+            benefice:
+            nombreValide(data.benefice),
+
+            prixRevente:
+            nombreValide(data.prixRevente),
+
+            prixUnitaire:
+            nombreValide(data.prixUnitaire)
+
+        });
+
+
+    });
+
+
+
+    return produits;
+
+}
+
+
+
+
+export async function ajouterProduit(
+    utilisateurConnecte,
+    produit
+){
+
+    if(!utilisateurValide(auth, utilisateurConnecte))
+        return;
+
+
+
+    await addDoc(
+
+        collection(db,"produits"),
+
+        {
+
+            ...produit,
+
+            userId:
+            auth.currentUser.uid,
+
+            dateAjout:
+            serverTimestamp()
+
+        }
+
+    );
+
+
+}
+
+
+
+export async function supprimerProduit(
+    utilisateurConnecte,
+    id
+){
+
+
+    if(!utilisateurValide(auth, utilisateurConnecte))
+        return;
+
+
+
+    await deleteDoc(
+
+        doc(
+            db,
+            "produits",
+            id
+        )
+
+    );
+
+}
