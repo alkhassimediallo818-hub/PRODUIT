000300aa028d6d61538875b41dd3975c929746e4
Qassimedev <alkhassimediallo818@gmail.com>
2026-07-17 11:15:14 +0000

    dashboard.js

--- JS/dashboard.js
+++ JS/dashboard.js
@@ -87,3 +87,101 @@ export function mettreAJourResume(produits, ventesGlobales){
 
 
 }
+// ===============================
+// RESUME VENTES
+// ===============================
+
+export function calculerResumeVentes(ventesGlobales){
+
+
+    let ca = 0;
+
+    let benefice = 0;
+
+    let unites = 0;
+
+
+
+    ventesGlobales.forEach((vente)=>{
+
+
+        ca +=
+
+        nombreValide(
+            vente.montantTotal
+        );
+
+
+
+        benefice +=
+
+        nombreValide(
+            vente.benefice
+        );
+
+
+
+        unites +=
+
+        nombreValide(
+            vente.quantiteVendue
+        );
+
+
+    });
+
+
+
+    const elements = {
+
+
+        resumeVentes:
+        unites,
+
+
+        resumeCA:
+        ca + " FCFA",
+
+
+        resumeBenefice:
+        benefice + " FCFA",
+
+
+        unitesVendues:
+        unites,
+
+
+        nbTransactions:
+        ventesGlobales.length
+
+
+    };
+
+
+
+    Object.keys(elements)
+    .forEach((id)=>{
+
+
+        const element =
+
+        document.getElementById(id);
+
+
+
+        if(element)
+
+            element.textContent =
+            elements[id];
+
+
+    });
+
+
+
+    afficherProduitVedette(
+        ventesGlobales
+    );
+
+
+}
