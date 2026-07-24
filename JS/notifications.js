f043f00b59592a7f124a16fbab0b5bbf81b14713
Qassimedev <alkhassimediallo818@gmail.com>
2026-07-24 02:07:48 +0100

    Create notifications.js

---
+++ JS/notifications.js
@@ -0,0 +1,19 @@
+afficherNotification(
+    "Produit ajouté",
+    "success"
+);
+
+afficherNotification(
+    "Stock faible",
+    "warning"
+);
+
+afficherNotification(
+    "Erreur Firestore",
+    "error"
+);
+
+afficherNotification(
+    "Information",
+    "info"
+);
