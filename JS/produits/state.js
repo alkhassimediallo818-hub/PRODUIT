f981a2a645ab15f36d77bd8692fcd6cf9914ec9b
Qassimedev <alkhassimediallo818@gmail.com>
2026-07-27 13:25:41 +0100

    Add state management for produitModification

---
+++ JS/produits/state.js
@@ -0,0 +1,22 @@
+let produitModification = null;
+
+
+export function getProduitModification(){
+
+    return produitModification;
+
+}
+
+
+export function setProduitModification(id){
+
+    produitModification = id;
+
+}
+
+
+export function resetProduitModification(){
+
+    produitModification = null;
+
+}
